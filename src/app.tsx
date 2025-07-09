d623fe1b456d63593ee55a449c75b3c148dbea51
Marcus Santos <marcuscruzsantos27@gmail.com>
2025-07-09 20:35:19 -0300

    feat: add listing card, room creation form and room page style

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CreateRoom from './pages/create-room';
-import Room from './pages/room';
+import { Room } from './pages/room';
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<CreateRoom />} index />
-          <Route element={<Room />} path="/room/:id" />
+          <Route element={<Room />} path="/room/:roomId" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
